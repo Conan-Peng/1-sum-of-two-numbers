76ea25f79dbeaf533bb9d40c8b06c154a6325fbd
Conan-Peng <56782233+Conan-Peng@users.noreply.github.com>
2020-02-05 02:53:45 +0800

    Create 101.symmetric tree recursively.cpp

---
+++ 101.symmetric tree recursively.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return isSymmetric(root->left, root->right);
+        
+    }
+    bool isSymmetric(TreeNode* left, TreeNode* right) {
+        if (!left && !right) return true;
+        if ( !left && right || left && !right || left->val != right->val ) return false;
+        return isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left);
+
+    }
+};
