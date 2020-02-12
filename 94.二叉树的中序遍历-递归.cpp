24cbee2e5e64e6af009d1fc45f7903129b661b25
Conan-Peng <56782233+Conan-Peng@users.noreply.github.com>
2020-02-12 10:40:30 +0800

    Create 94.二叉树的中序遍历-递归.cpp

---
+++ 94.二叉树的中序遍历-递归.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        //递归实现
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+    void inorder(TreeNode* root, vector<int>& res){
+        if (!root) return;
+        if (root->left) inorder(root->left, res);
+        res.push_back(root->val);
+        if (root->right) inorder(root->right, res);
+    }
+};
