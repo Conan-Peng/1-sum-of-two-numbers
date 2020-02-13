c6377ed7d8efa632053d1b3d4809e37323f7408a
Conan-Peng <56782233+Conan-Peng@users.noreply.github.com>
2020-02-14 07:54:04 +0800

    Create 94.inorder traversal iteratively.cpp

---
+++ 94.inorder traversal iteratively.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        //迭代实现
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode* p = root;
+        while (p || !s.empty())
+        {
+            while (p)
+            {
+                s.push(p);
+                p = p->left;
+            }
+            p = s.top(); s.pop();
+            res.push_back(p->val);
+            p = p->right;
+        }
+        return res;
+    }
+};
