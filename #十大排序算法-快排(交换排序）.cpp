ac5200449e36ddd3237263aeb38ae8bd866d6217
Conan-Peng <ppp20111@163.com>
2020-04-30 07:10:41 +0800

    Update #十大排序算法-快排(交换排序）.cpp

--- #十大排序算法-快排(交换排序）.cpp
+++ #十大排序算法-快排(交换排序）.cpp
@@ -1,50 +1,41 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
-class Qsort{
-public:
-    void quickSort(vector<int>& a, int leftEnd, int rightEnd){
-        if (leftEnd >= rightEnd) return;
-
-        int pivot = a[leftEnd];
-        int leftCursor = leftEnd; //左开右闭区间，最左边的是支点取不到。
-        int rightCursor = rightEnd + 1;//警醒！一般编译通过的运行异常都是--指针指向未知空间！
-
-        while(true)
-        {
-            do
-            {
-                leftCursor++;
-            } while (a[leftCursor] < pivot); //左边却不小于支点的索引
-            do
-            {
-                rightCursor--;
-            } while (a[rightCursor] > pivot); //右边却不大于支点的索引
-            
-            if (leftCursor >= rightCursor) break;
-            swap(a[leftCursor], a[rightCursor]); //交换
-        }
-        //放置支点
-        a[leftEnd] = a[rightCursor];
-        a[rightCursor] = pivot;
-
-        //对左右数段分别快排
-        quickSort(a, leftEnd, rightCursor - 1);
-        quickSort(a, rightCursor + 1, rightEnd);
+void qSort(vector<int>& vec, int leftEnd, int rightEnd){
+    if (leftEnd >= rightEnd)
+    {
+        return;
     }
-};
+    int pivot = leftEnd; //支点
+    int leftCursor = leftEnd; //左游标
+    int rightCursor = rightEnd + 1; //右游标，左开右闭
 
-int main()
-{
-    int c[8] = {1,3,4,5,2,1,4,5};
-	vector<int> A(c, c+8);
-    Qsort qs;
-	qs.quickSort(A, 0, A.size() - 1);
-	for (int b = 0; b < A.size(); b++)
-	{
-		cout << A[b] << " ";
-	}
-    return 0;
+    while (true)
+    {
+        do
+        {
+            leftCursor++;
+        } while (vec[leftCursor] < vec[pivot]);
+        do
+        {
+            rightCursor--;
+        } while (vec[rightCursor] > vec[pivot]);
+        if (leftCursor >= rightCursor) break;
+        swap(vec[leftCursor], vec[rightCursor]);           
+    }
+    swap(vec[pivot], vec[rightCursor]);//放置支点
+    qSort(vec, leftEnd, rightCursor-1);
+    qSort(vec, rightCursor+1, rightEnd);
+}
+
+int main(){
+    vector<int> a{5,3,6,11,432,4,68,6,6,6,7,9};
+    qSort(a, 0, a.size() - 1);
+    for (auto a0 : a)
+    {
+        cout << a0 << ' ';
+    }
 }
